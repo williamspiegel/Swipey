055711714a220bee6112eb3eb85978927ceb4804
William Spiegel <willy.spiegel@gmail.com>
2020-12-31 23:31:39 -0500

    Carousel image, image gallery, video, and text markdown prototy[e

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {
+  Image,
+  MaskedViewBase,
+  Modal,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {QueryObserverResult, useQuery, useQueryClient} from 'react-query';
 import axios from 'axios';
-import ReactPlayer from 'react-player';
+import he from 'he';
 import WebView from 'react-native-webview';
+import VideoPlayer from 'expo-video-player';
+import {
+  heightPercentageToDP,
+  widthPercentageToDP,
+} from 'react-native-responsive-screen';
+import Carousel from 'react-native-snap-carousel';
+import {Audio, Video} from 'expo-av';
+import Markdown from 'react-native-markdown-display';
+import ImageViewer from 'react-native-image-zoom-viewer';
 const config: any = {
   method: 'post',
   url:
@@ -16,38 +33,151 @@ const config: any = {
 
 const Home = () => {
   const queryClient = useQueryClient();
-  const {data: anonymous} = useQuery('anonymous', () => {
-    axios(config)
-      .then(function (response: {data: any}) {
-        console.log(JSON.stringify(response.data));
-      })
-      .catch(function (error: any) {
-        console.log(error);
-      });
-  });
-
-  const {data: subData} = useQuery(
-    'subData' + anonymous?.access_token,
-    () => {
+  const {data: anon} = useQuery('anon', () => axios(config));
+  const anonTok = anon?.data?.access_token;
+  const {data: subData, isFetched} = useQuery(
+    'subData' + anonTok,
+    () =>
       axios({
         method: 'get',
-        url: 'https://oauth.reddit.com/r/all/hot?g=US&after&limit=100',
+        url: 'https://oauth.reddit.com/r/dogelore/hot?g=US&after&limit=100',
         headers: {
-          Authorization: `bearer ${anonymous?.access_token}`,
+          Authorization: `bearer ${anonTok}`,
           'User-Agent': 'Swipey for Reddit',
         },
-      })
-        .then(function (response) {
-          console.log(JSON.stringify(response.data));
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    },
-    {enabled: anonymous?.access_token},
+      }),
+    {enabled: !!anonTok},
   );
 
-  return <>{/*<WebView source={{html: ExampleData}} />*/}</>;
+  //children[0].data.media.oembed.html
+  // console.log(
+  //   'lolwut:   ',
+  //   he
+  //     .unescape(
+  //       '&lt;iframe width="356" height="200" src="https://www.youtube.com/embed/zdVgek9JJdI?feature=oembed&amp;enablejsapi=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen&gt;&lt;/iframe&gt;',
+  //     )
+  //     .match(/src="(?<src>[^\"]*)"/)?.groups['src'],
+  // );
+  const [imgVisible, setimgVisible] = useState(false);
+  const [currentUri, setcurrentUri] = useState('');
+  const contentDisplay = (item: any) => {
+    if (item?.data?.post_hint === 'image') {
+      return <Image style={{flex: 4}} source={{uri: item.data.url}} />;
+    } else if (item?.data?.is_self === true) {
+      return <Markdown>{he.unescape(item?.data?.selftext)}</Markdown>;
+    } else if (item?.data?.is_gallery === true) {
+      let meta = item?.data?.media_metadata;
+      let gallery = item?.data?.gallery_data?.items?.reduce((acc, c) => {
+        acc.push({
+          url:
+            'https://i.redd.it/' +
+            c.media_id +
+            (meta[`${c.media_id}`].m === 'image/jpg' ? '.jpg' : '.png'),
+        });
+        return acc;
+      }, []);
+      setcurrentUri(`${gallery[0].url}`);
+      return (
+        <>
+          <Image source={{uri: currentUri}} />
+          <Modal visible={imgVisible} transparent={true}>
+            <ImageViewer
+              imageUrls={gallery}
+              enableSwipeDown
+              swipeDownThreshold={50}
+              enablePreload
+              onSwipeDown={() => {
+                setimgVisible(false);
+              }}
+              onChange={(i?: number) => {
+                if (i) {
+                  setcurrentUri(gallery[i]);
+                }
+              }}
+            />
+          </Modal>
+        </>
+      );
+    } else if (item?.data?.post_hint === 'link') {
+      return <Text>insert link post</Text>;
+    } else if (item?.data?.secure_media?.reddit_video?.fallback_url) {
+      let vid = item.data.secure_media.reddit_video.fallback_url;
+      let re = /(DASH_).*(\.mp4)/;
+      let audioURL = vid.replace(re, '!audio!');
+      return (
+        <VideoPlayer
+          videoProps={{
+            shouldPlay: false,
+            resizeMode: Video.RESIZE_MODE_CONTAIN,
+            source: {
+              uri: vid,
+            },
+          }}
+          showFullscreenButton
+          width={widthPercentageToDP(100)}
+          height={(widthPercentageToDP(100) * 9) / 16}
+          inFullscreen={true}
+        />
+      );
+    } else if (item?.data?.media_embed?.content) {
+      <WebView
+        scrollEnabled={false}
+        allowsFullscreenVideo
+        originWhitelist={['*']}
+        source={{
+          uri: he
+            .unescape(item.data.media_embed.content)
+            .match(/src="(?<src>[^\"]*)"/)?.groups['src'],
+        }}
+        style={styles.loginWebView}
+      />;
+    } //else if (true) {
+    // }
+    else {
+      return <Text>Content not Found</Text>;
+    }
+  };
+  const _renderItem = ({item, index}: any) => {
+    console.log(`current item at index ${index}:   `, item);
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <Text>{item?.data?.title}</Text>
+          {contentDisplay(item)}
+        </View>
+        <View style={{flex: 1, backgroundColor: 'red'}}></View>
+      </ScrollView>
+    );
+  };
+  const carouselRef: any = useRef('');
+  //console.log('sub data:   ', subData);
+  return (
+    <View style={{flex: 1, backgroundColor: 'blue'}}>
+      {isFetched && (
+        <Carousel
+          ref={carouselRef}
+          data={subData?.data?.data?.children}
+          renderItem={_renderItem}
+          layoutCardOffset={50}
+          inactiveSlideOpacity={1}
+          sliderHeight={heightPercentageToDP(100)}
+          itemHeight={heightPercentageToDP(100)}
+          sliderWidth={widthPercentageToDP(100)}
+          itemWidth={widthPercentageToDP(100)}
+        />
+      )}
+    </View>
+  );
 };
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    width: widthPercentageToDP(100),
+    height: (widthPercentageToDP(100) * 9) / 16,
+  },
+  loginWebView: {
+    flex: 1,
+  },
+});
 
 export default Home;
